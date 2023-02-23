ea7bdcea86c3c3c7515eb7b6aab9b59bac5d0f47
Wong Hong You <wonghongyou@gmail.com>
2023-02-23 22:36:15 +0800

    ex00 working

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -14,15 +14,15 @@
 
 int main(int ac, char **av) {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE DEEFBACK NOISE *"
+		std::cout << "* LOUD AND UNBEARABLE DEEFBACK NOISE *";
 	else {
 		for (int i = 1; av[i]; i++) {
-			for (int j = 0; ac[i][j]; j++)
+			for (int j = 0; av[i][j]; j++)
 				std::cout << (char) toupper(av[i][j]);
 			if (i < ac - 1)
 				std::cout << " ";
 		}
 	}
-	std::cout >> std::endl;
-	return (EXIT_SUCCESS);
+	std::cout << std::endl;
+	return (0);
 }

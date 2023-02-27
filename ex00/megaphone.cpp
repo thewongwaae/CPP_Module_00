e5c21f45772436b50947c201b6bbc9d9f15474d6
Hong You Wong <hwong@u90z06s02.42kl.edu.my>
2023-02-27 14:49:53 +0800

    slight changes to ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -12,9 +12,9 @@
 
 #include <iostream>
 
-int main(int ac, char **av) {
+int main( int ac, char **av ) {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE DEEFBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else {
 		for (int i = 1; av[i]; i++) {
 			for (int j = 0; av[i][j]; j++)
